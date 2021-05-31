301e142cd2cf148d505481fd33b6ff8064c42580
Akashcba <akashcba2022@email.iimcal.ac.in>
2021-05-31 12:58:00 +0000

    Finish Commit

--- Melanoma/main.py
+++ Melanoma/main.py
@@ -38,7 +38,7 @@ def train(fold):
     training_data_path = "path"
     model_path = "directory"
     df = pd.read_csv("train_folds.csv")
-    devide = "cuda" ## whether cuda is there or not
+    device = "cuda" ## whether cuda is there or not
     epochs = 50
     train_bs = 32 # bs = batch_size
     valid_bs = 16
@@ -48,7 +48,7 @@ def train(fold):
     ## Normalize the images
     ## using the albumentation library
     mean=(0.485, 0.456, 0.406)
-    std=(0.229, 0.224. o.225)
+    std=(0.229, 0.224 , 0.225)
 
     train_aug = albumentations.Compose(
         [
@@ -133,15 +133,71 @@ def train(fold):
             optimizer,
             device
         )
+
         ## Metrics
-        predicts = np.vstack(predictions).ravel()
+        predicts = np.vstack((predictions)).ravel()
         auc = metrics.roc_auc_score(valid_targets, predictions)
         scheduler.step(auc)
         print(f"epoch = {epoch}, auc = {auc}")
-        es(auc, model, model_path)
+        es(auc, model, os.path.join(model_path, f"model{fold}.bin"))
         if es.early_stop:
             print("Early Stopping")
             break
 
+
+def predict(fold):
+    test_data_path = "path"
+    model_path = "directory"
+    df_test = pd.read_csv("testfile.csv")
+    df_test.loc[:, "target"] = 0 # Prediction column
+    device = "cuda" ## whether cuda is there or not
+    epochs = 50
+    test_bs = 32 # bs = batch_size
+    valid_bs = 16
+
+    ## Normalize the images
+    ## using the albumentation library
+    mean=(0.485, 0.456, 0.406)
+    std=(0.229, 0.224 , 0.225)
+
+    test_aug = albumentations.Compose(
+        [
+            albumentations.Normalize(mean, std, max_pixel_value=255.0, always_apply=True
+            )
+        ]
+    )
+
+    test_images = df_test.image_name.values.tolist()
+    test_images = [os.path.join(training_data_path, i+".jpg") for i in train_images] ## List of image paths
+    test_targets = df_test.target.values
+
+    test_dataset = ClassificationLoader(
+        image_paths=test_images,
+        targets=test_targets,
+        resize=None,
+        augmentations=test_aug
+    )
+
+    
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset,
+        batch_size=test_bs,
+        shuffle=False,  # Set False won't return targets if True
+        num_workers=4
+    )
+
+    model = SEResNext50_32x4d(pretrained="imagenet")
+    model.load_state_dict(torch.load(os.path.join(model_path, f"model{fold}.bin")))
+    model.to(device)
+
+    predictions = Engine.predict(
+        test_loader,
+        model,
+        device
+    )
+
+    return np.vstack((predictions)).ravel()
+
 if __name__=="__main__":
     train(fold=0)
+    predict(fold=0)
