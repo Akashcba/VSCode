60bc7d6cbad3831a8695d8be653d519da6d8aea1
Akashcba <akashcba2022@email.iimcal.ac.in>
2021-06-01 07:25:45 +0000

    model.py

---
+++ FADML/model.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+import argparse
+import torch
+from torch.nn import Linear, ReLU, Sequential, Conv2d, MaxPool2d
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+from torchvision import datasets, transforms
+from torch.optim.lr_scheduler import StepLR
+import argparse
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.cnn_layers = Sequential(
+            # Defining a 2D convolution layer
+            Conv2d(3, 6, kernel_size=5, stride=1),
+            ReLU(inplace=True),
+            MaxPool2d(2,2),
+            # Defining another 2D convolution layer
+            Conv2d(6, 6, kernel_size=5, stride=1),
+            ReLU(inplace=True),
+            MaxPool2d(2,2)
+        )
+        self.linear_layers = Sequential(
+            Linear(6*5*5, 120)
+            Linear(120, 84)
+            Linear(84, 10)
+            )
+
+    # Defining the forward pass    
+    def forward(self, x):
+        x = self.cnn_layers(x)
+        x = x.view(x.size(0), -1)
+        x = self.linear_layers(x)
+        return x
+ 
+
+def convfc():
+    model_arch = Net()
+    return model_arch
