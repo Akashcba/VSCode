5671f01782b6cf79b21a767a69361ad25f1220d7
AkashCba <akashcba2022@email.iimcal.ac.in>
2021-06-05 13:29:31 +0000

    model file added

--- DL/test.py
+++ DL/test.py
@@ -10,3 +10,16 @@ import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 import random
 
+import models
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+model = models.Model(
+    eng_vocab_size=5763,
+    hin_vocab_size=9840,
+    embedding_size = 256,
+    hidden_size = 256,
+    layers = 1,
+    bidirection = True)
+model.load_state_dict(torch.load())
+model.eval()
